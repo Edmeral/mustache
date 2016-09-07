e68b74ee40b7dc60c9638309c28229c18f7a9379
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2016-09-07 16:34:32 -0400

    Taking pictures

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+// Displaying stream from the camera to page
+const height = 640, width = 480
+const video = document.querySelector('video')
+
 const errCallback = e => console.log('Better luck next time!')
 const successCallback = localMediaStream => {
-  let video = document.querySelector('video')
   video.src  = window.URL.createObjectURL(localMediaStream)
+  window.localMediaStream = localMediaStream
 }
 
 navigator.getUserMedia(
-  { video: true, audio: true }, 
+  { video: 
+    { mandatory: { maxHeight: height, maxWidth: width } 
+    },
+    audio: false }, 
   successCallback, 
   errCallback
 )
 
+// taking a picture
+const canvas = document.querySelector('canvas')
+canvas.width = width
+canvas.height = height
+const ctx = canvas.getContext('2d')
+const snapshot = () => {
+  if (window.localMediaStream) {
+    ctx.drawImage(video, 0, 0)
+    let src = canvas.toDataURL('image/png')
+    document.querySelector('img').src = src
+    // window.location.href = src // downloading
+  }
+}
+
+document.querySelector('button').addEventListener('click', snapshot)
